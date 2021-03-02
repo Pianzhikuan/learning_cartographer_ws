f626bf52e406bf5934a73eb23eb02c143413fd01
Pianzhikuan <1301519350@qq.com>
2021-03-02 09:41:06 +0800

    测试成功

---
+++ devel_isolated/env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# generated from catkin.builder Python module
+
+/home/monomania/cartographer_ws/devel_isolated/ceres-solver/env.sh "$@"
---
+++ devel_isolated/setup.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+# generated from catkin.builder Python module
+
+. "/home/monomania/cartographer_ws/devel_isolated/ceres-solver/setup.zsh"
